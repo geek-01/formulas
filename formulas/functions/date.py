2c770ed82d456e45bcdf5fe024e3293f80da1dc9
Nicholas Bollweg <nick.bollweg@gmail.com>
2019-05-04 15:06:21 -0400

    Added Date functions

--- formulas/functions/date.py
+++ formulas/functions/date.py
@@ -14,21 +14,21 @@ def _str(text):
 
 
 def find_day(date_str):
-    return dparser.parse(_str(date_str), fuzzy=True, dayfirst=True).day
+    return _str(dparser.parse(_str(date_str), fuzzy=True, dayfirst=True).day)
 
 
 FUNCTIONS['DAY'] = wrap_ufunc(find_day)
 
 
 def find_month(date_str):
-    return dparser.parse(_str(date_str), fuzzy=True, dayfirst=True).month
+    return _str(dparser.parse(date_str, fuzzy=True, dayfirst=True).month)
 
 
 FUNCTIONS['MONTH'] = wrap_func(find_month)
 
 
 def find_year(date_str):
-    return dparser.parse(_str(date_str), fuzzy=True, dayfirst=True).year
+    return _str(dparser.parse(_str(date_str), fuzzy=True, dayfirst=True).year)
 
 
 FUNCTIONS['YEAR'] = wrap_func(find_year)
