7a433d43fbe6f4a0158b9f3cc3ad6f8976bb729c
Nicholas Bollweg <nick.bollweg@gmail.com>
2019-05-04 15:06:21 -0400

    Added Date functions

--- formulas/functions/date.py
+++ formulas/functions/date.py
@@ -1,4 +1,5 @@
 import dateutil.parser as dparser
+from dateutil.parser import parse
 import datetime
 from . import (
     wrap_func
@@ -27,5 +28,39 @@ def find_year(date_str):
 
 
 FUNCTIONS['YEAR'] = wrap_func(find_year)
-FUNCTIONS['NOW'] = wrap_func(datetime.datetime.now().strftime("%Y-%m-%d %I:%M:%S %p"))
-FUNCTIONS['TODAY'] = wrap_func(datetime.datetime.now().strftime("%Y-%m-%d"))
+
+
+def xnow():
+    return datetime.datetime.now().strftime("%Y-%m-%d %I:%M:%S %p")
+
+
+FUNCTIONS['NOW'] = wrap_func(xnow)
+
+
+def xtoday():
+    return datetime.datetime.now().strftime("%Y-%m-%d")
+
+
+FUNCTIONS['TODAY'] = wrap_func(xtoday)
+
+
+def xisdate(date_str):
+    try:
+        parse(date_str, fuzzy=False)
+        return "True"
+    except ValueError:
+        return "False"
+
+
+FUNCTIONS['ISDATE'] = wrap_func(xisdate)
+
+
+def xgetdate(date_str):
+    try:
+        date = dparser.parse(date_str, fuzzy=True)
+        return date.date().strftime("%Y-%m-%d")
+    except ValueError:
+        raise Exception("Incorrect Date format!")
+
+
+FUNCTIONS['DATE'] = wrap_func(xgetdate)
