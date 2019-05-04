6f3dab8e026c172eca8913a5ea0b5e90233c5791
Nicholas Bollweg <nick.bollweg@gmail.com>
2019-05-04 15:06:21 -0400

    Added Date functions

--- formulas/functions/date.py
+++ formulas/functions/date.py
@@ -2,6 +2,7 @@ import math
 import functools
 import numpy as np
 import datetime
+from . import wrap_ufunc, Error, replace_empty, XlError, value_return
 import dateutil.parser as dparser
 from . import (
     get_error, raise_errors, is_number, flatten, wrap_ufunc, wrap_func,
@@ -10,9 +11,17 @@ from . import (
 
 # noinspection PyDictCreation
 FUNCTIONS = {}
-_kw0 = dict(
-    input_parser=lambda *a: a,
-    args_parser=lambda *a: map(functools.partial(replace_empty, empty=''), a)
+
+
+def _str(text):
+    if isinstance(text, bool):
+        return str(text).upper()
+    return str(text)
+
+
+_kw1 = dict(
+    input_parser=lambda text: [_str(text)], return_func=value_return,
+    args_parser=lambda *a: map(functools.partial(replace_empty, empty=''), a),
 )
 
 
@@ -20,18 +29,18 @@ def find_day(date_str):
     return dparser.parse(date_str, fuzzy=True, dayfirst=True).day
 
 
-FUNCTIONS['DAY'] = wrap_ufunc(find_day, **_kw0)
+FUNCTIONS['DAY'] = wrap_ufunc(find_day, **_kw1)
 
 
 def find_month(date_str):
     return dparser.parse(date_str, fuzzy=True, dayfirst=True).month
 
 
-FUNCTIONS['DAY'] = wrap_func(find_month, **_kw0)
+FUNCTIONS['DAY'] = wrap_func(find_month, **_kw1)
 
 
 def find_year(date_str):
     return dparser.parse(date_str, fuzzy=True, dayfirst=True).year
 
 
-FUNCTIONS['DAY'] = wrap_func(find_year, **_kw0)
+FUNCTIONS['DAY'] = wrap_func(find_year, **_kw1)
